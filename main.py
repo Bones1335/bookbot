837e2a278569e4048cdec96c6aaa437a07221145
Alexander Hansen <ahansen1335@gmail.com>
2025-02-24 09:42:55 +0100

    updated boot.dev curriculum for bookbot

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    chars_dict = get_num_chars(text)
-    chars_list = list(chars_dict.items())
-    sorted_chars = sorted(chars_list, key=sort_on, reverse=True)
-    print(f"---Begin Report of {book_path}---")
-    print(f"{num_words} words found in the document")
-    print("")
-    for char in sorted_chars:
-        if char[0].isalpha():
-            print(f"The '{char[0]}' character was found {char[1]} times")
-    print("---End Report---")
-
-
-def sort_on(list):
-    return list[1]
-
-
-def get_num_chars(text):
-    words = text.split()
-    chars_dict = {}
-    for word in words:
-        chars = list(word)
-        for char in chars:
-            lower_char = char.lower()
-            if lower_char not in chars_dict:
-                chars_dict[lower_char] = 1
-            elif lower_char in chars_dict:
-                chars_dict[lower_char] += 1
-
-    return chars_dict
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
